2453c80d6e5d066e865bc18e69c86364cf9fbe6e
Nicolas Garza <nicolas.garza@gmail.com>
2024-06-01 23:07:34 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-# Uncomment this to pass the first stage
 import socket
 
 
@@ -6,10 +5,8 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    server_socket.accept()[0].sendall("HTTP/1.1 200 OK\r\n\r\n".encode())
 
 
 if __name__ == "__main__":
