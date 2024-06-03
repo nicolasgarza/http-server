3bbaace66049f875f1be8052df9f63e4d3cacb32
Nicolas Garza <nicolas.garza@gmail.com>
2024-06-02 21:48:02 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,15 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall("HTTP/1.1 200 OK\r\n\r\n".encode())
+    conn, _ = server_socket.accept()
+    data = conn.recv(1024).decode()
+    print("recieved: " + data)
+    parsed = data.split(" ")[1]
+    print("parsed: " + parsed)
+    if parsed == "/":
+        conn.sendall("HTTP/1.1 200 OK\r\n\r\n".encode())
+    else:
+        conn.sendall("HTTP/1.1 404 Not Found\r\n\r\n".encode())
 
 
 if __name__ == "__main__":
