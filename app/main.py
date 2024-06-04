14fcc3bc12b5ef9a9fff2a427485ae89a63e78f3
Nicolas Garza <nicolas.garza@gmail.com>
2024-06-04 14:55:51 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HTTP_400_BAD_REQUEST = "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\nContent-Length: 11\r\n\r\nBad Request"
 HTTP_404_NOT_FOUND = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found"
@@ -9,12 +10,14 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn, _ = server_socket.accept()
+    while True:
+        conn, _ = server_socket.accept()
 
-    data = conn.recv(1024).decode()
-    print("recieved: " + data)
+        data = conn.recv(1024).decode()
+        print("recieved: " + data)
 
-    route_req(conn, data)
+        thread = threading.Thread(target=route_req, args=(conn, data))
+        thread.start()
 
 
 def route_req(conn, parsed_data):
