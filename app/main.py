adcb5f237f7a177e3f551fcd63e0261336249b93
Nicolas Garza <nicolas.garza@gmail.com>
2024-06-12 22:15:31 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import os.path
 
 HTTP_400_BAD_REQUEST = "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\nContent-Length: 11\r\n\r\nBad Request"
 HTTP_404_NOT_FOUND = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nNot Found"
+VALID_ENCODINGS = {"gzip"}
 
 file_dir = sys.argv[-1]
 
@@ -18,7 +19,7 @@ def main():
         conn, _ = server_socket.accept()
 
         data = conn.recv(1024).decode()
-        print("recieved: " + data)
+        print("recieved: " + repr(data))
 
         thread = threading.Thread(target=route_req, args=(conn, data))
         thread.start()
@@ -29,10 +30,15 @@ def route_req(conn, raw_data):
     parsed_data = raw_data.split(" ")
     print(parsed_data)
     if parsed_data[1] == "/":
-        conn.sendall(build_http_req("200", "OK", "text/plain", "OK").encode())
+        conn.sendall(
+            build_http_req("200", "OK", "OK", {"Content-Type": "text/plain"}).encode()
+        )
 
     elif parsed_data[1].startswith("/echo"):
-        conn.sendall(handle_echo(parsed_data[1]).encode())
+        if raw_data.find("Accept-Encoding") == -1:
+            conn.sendall(handle_echo(parsed_data[1]).encode())
+        else:
+            conn.sendall(handle_compressed_echo(raw_data).encode())
 
     elif parsed_data[1].startswith("/user-agent"):
         conn.sendall(handle_user_agent(parsed_data).encode())
@@ -48,17 +54,19 @@ def route_req(conn, raw_data):
 
 def handle_echo(parsed_data):
     parsed = parsed_data.split("/")
-    return build_http_req("200", "OK", "text/plain", parsed[2])
+    print("parsed data for echo: ")
+    print(parsed)
+    return build_http_req("200", "OK", parsed[2], {"Content-Type": "text/plain"})
 
 
 def handle_user_agent(parsed_data):
     parsed = parsed_data[-1].rstrip("\r\n")
-    return build_http_req("200", "OK", "text/plain", parsed)
+    return build_http_req("200", "OK", parsed, {"Content-Type": "text/plain"})
 
 
 def handle_get_file(file_path):
     global file_dir
-    path = f"{file_dir}{file_path.lstrip("/files")}"
+    path = f"{file_dir}{file_path[len('/files'):]}"
     print("target file path:", path)
     if not os.path.isfile(path):
         print("file does not exist")
@@ -66,7 +74,9 @@ def handle_get_file(file_path):
 
     target_file = open(path, "r")
     text = target_file.read()
-    return build_http_req("200", "OK", "application/octet-stream", text)
+    return build_http_req(
+        "200", "OK", text, {"Content-Type": "application/octet-stream"}
+    )
 
 
 def handle_post_file(parsed_data):
@@ -74,16 +84,37 @@ def handle_post_file(parsed_data):
     path = os.path.join(file_dir, parsed_data.split(" ")[1].lstrip("/files"))
     print("target file path", path)
     print("data to work with: " + repr(parsed_data))
+
     write_data = parsed_data.split("\r\n")[-1]
     print("write data:", write_data)
+
     f = open(path, "a")
-    f.write(write_data.lstrip("b'"))
+    f.write(write_data)
     f.close()
-    return build_http_req("201", "Created", "text/plain", "Created")
+    return build_http_req("201", "Created", "Created", {"Content-Type": "text/plain"})
 
 
-def build_http_req(code, reason, content_type, msg):
-    return f"HTTP/1.1 {code} {reason}\r\nContent-Type: {content_type}\r\nContent-Length: {len(msg)}\r\n\r\n{msg}"
+def handle_compressed_echo(parsed_data):
+    lines = parsed_data.split("\r\n")
+    print(lines)
+    accept_encoding = None
+    for line in lines:
+        if line.startswith("Accept-Encoding:"):
+            accept_encoding = line.split(":", 1)[1].strip()
+            print(accept_encoding)
+            break
+    headers = {"Content-Type": "text/plain"}
+    if accept_encoding and accept_encoding in VALID_ENCODINGS:
+        headers["Content-Encoding"] = accept_encoding
+
+    return build_http_req("200", "OK", "foo", headers=headers)
+
+
+def build_http_req(code, reason, msg, headers=None):
+    if headers is None:
+        headers = {}
+    optional_headers = "\r\n".join(f"{key}: {value}" for key, value in headers.items())
+    return f"HTTP/1.1 {code} {reason}\r\n{optional_headers}\r\nContent-Length: {len(msg)}\r\n\r\n{msg}"
 
 
 if __name__ == "__main__":
